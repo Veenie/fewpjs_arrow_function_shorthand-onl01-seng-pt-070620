bed13c39e5e579a4544b3d61e0e643122aa6667c
Raveen K Sharma <raveen08@gmail.com>
2021-02-06 20:57:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let divide = function () {return 2000 / 100}
 
-let square = x => (x ** 2);
+//written this way, needs return, will not do auto like arrow
+
+let square = x => x ** 2;
 
 let add = (a, b) => a + b; 
 
